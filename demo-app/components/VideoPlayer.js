12d4f1c6b764329ebde3284d67437f248d5c512c
JavidHaji-zada <JavidHaji-zada@users.noreply.github.com>
2020-02-21 13:50:49 +0300

    video react integrated

--- demo-app/components/VideoPlayer.js
+++ demo-app/components/VideoPlayer.js
@@ -1,13 +1,19 @@
 import PropTypes from 'prop-types';
 import React, { Component } from 'react';
+import { Player, BigPlayButton, PlayToggle, ReplayControl, ForwardControl, VolumeMenuButton, PlaybackRateMenuButton, ControlBar } from 'video-react';
+import 'video-react/dist/video-react.css'; // import css
 
 class VideoPlayer extends Component {
   componentDidMount() {
-    this.video.addEventListener('play', () => this.startInterval());
-    this.video.addEventListener('pause', () => this.stopInterval());
-    this.video.addEventListener('seeked', () => this.updateTime());
+    // this.video.addEventListener('play', () => this.startInterval());
+    // this.video.addEventListener('pause', () => this.stopInterval());
+    // this.video.addEventListener('seeked', () => this.updateTime());
+    this.player.subscribeToStateChange(this.handleStateChange.bind(this));
   }
-
+  handleStateChange(state, prevState) {
+    // copy player state to this component's state
+    this.props.onTimeUpdate(state.currentTime)
+  }
   startInterval() {
     this.interval = setInterval(() => {
       this.updateTime();
@@ -28,12 +34,21 @@ class VideoPlayer extends Component {
     /* eslint-disable jsx-a11y/media-has-caption */
     return (
       <div>
-        <video
-          ref={(c) => { this.video = c; }}
+        <Player
+          ref={(c) => { this.player = c; }}
           src={this.props.src}
           controls
           style={{ width: '100%' }}
-        />
+        >
+          <BigPlayButton position="center" />
+          <ControlBar autoHide={false} >
+            <PlayToggle />
+            <ReplayControl seconds={5} order={2.1} />
+            <ForwardControl seconds={5} order={3.1} />
+            <VolumeMenuButton vertical />
+            <PlaybackRateMenuButton rates={[2, 1.5, 1, 0.5, 0.1]} />
+          </ControlBar>
+        </Player>
       </div>
     );
     /* eslint-enable */
